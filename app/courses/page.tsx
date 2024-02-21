306aa13be12dbd8b8cbbd2d5975b52b26d30b15e
takudzwa17 <takuemujo@gmail.com>
2024-02-21 16:28:02 +0200

    removed search feature

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -13,8 +13,8 @@ import Footer from "../components/Footer";
 type Props = {};
 
 const Page = (props: Props) => {
-  const searchParams = useSearchParams();
-  const search = searchParams?.get("title");
+  //const searchParams = useSearchParams();
+  //const search = searchParams?.get("title");
   const { data, isLoading } = useGetUsersAllCoursesQuery(undefined, {});
   const { data: categoriesData } = useGetHeroDataQuery("Categories", {});
   const [route, setRoute] = useState("Login");
@@ -31,14 +31,16 @@ const Page = (props: Props) => {
         data?.courses.filter((item: any) => item.categories === category)
       );
     }
-    if (search) {
+    {
+      /**if (search) {
       setCourses(
         data?.courses.filter((item: any) =>
           item.name.toLowerCase().includes(search.toLowerCase())
         )
       );
+    } */
     }
-  }, [data, category, search]);
+  }, [data, category]);
 
   const categories = categoriesData?.layout.categories;
 
@@ -87,7 +89,7 @@ const Page = (props: Props) => {
                   </div>
                 ))}
             </div>
-            {courses && courses.length === 0 && (
+            {/**{courses && courses.length === 0 && (
               <p
                 className={`${styles.label} justify-center min-h-[50vh] flex items-center`}
               >
@@ -95,7 +97,7 @@ const Page = (props: Props) => {
                   ? "No courses found!"
                   : "No courses found in this category. Please try another one"}
               </p>
-            )}
+            )} */}
             <br />
             <br />
             <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] 1500px:grid-cols-4 1500px:gap-[35px] mb-12 border-0">
